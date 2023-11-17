aceb788e6c778d81118c33b4ccf9dcb89ebafa25
Luisa Correia <luisacorreia923@gmail.com>
2023-11-17 14:52:13 -0300

    Exercicio 4 Módulo de JavaScript

---
+++ JavaScript/calculadora03.js
@@ -0,0 +1,60 @@
+//Atividade de Funções - Exemplos de Calculadoras
+
+function calculadora (){
+    console.log("Digite os valores, e depois a operação desejada (+ - * /):");
+    let valor1 = parseFloat(window.prompt("Digite o primeiro valor:"));
+    let valor2 = parseFloat(window.prompt("Digite o segundo valor:"));
+    let operador = window.prompt("Digite o operador:");
+    let resultado;
+    switch(operador){
+        case "+": 
+            resultado = valor1 + valor2;
+            break;
+        case "-":
+            resultado = valor1 - valor2;
+            break;
+        case "*":
+            resultado = valor1 * valor2;
+            break;
+        case "/":
+            if(valor2 === 0){
+                resultado = null;
+            } else {
+                resultado = valor1/valor2;
+            }
+            break;
+        default:
+            resultado = "Operador Incorreto!";
+    }
+    return resultado;
+}
+
+function calculadora2(valor1,valor2,operador){
+    let resultado;
+    switch(operador){
+        case "+": 
+            resultado = valor1 + valor2;
+            break;
+        case "-":
+            resultado = valor1 - valor2;
+            break;
+        case "*":
+            resultado = valor1 * valor2;
+            break;
+        case "/":
+            if(valor2 === 0){
+                resultado = null;
+            } else {
+                resultado = valor1/valor2;
+            }
+            break;
+        default:
+            resultado = "Operador Incorreto!";
+    }
+    return resultado;
+}
+
+let resultado = (valor1,valor2,operador) => {
+
+    "O resultado é" + eval(`${valor1} ${operador} ${valor2}`);
+}
